7a0e0f8e480d29d3e096c485cc271de95c60827a
duquduq <dlstjs507@gmail.com>
2024-11-10 15:57:48 +0900

    Modigy app.py with new features

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
-
+import os
+from flask import Flask
+#수정했어용 3:56
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return "Hello, Render!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # PORT 환경 변수를 사용
+    app.run(host="0.0.0.0", port=port)        # 0.0.0.0으로 설정해 외부 접근 허용
