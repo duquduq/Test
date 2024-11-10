2a7bdae283f5c7ccf8bb79a9bfb41238dd0308d3
duquduq <dlstjs507@gmail.com>
2024-11-10 15:22:52 +0900

    주석

---
+++ static/script.js
@@ -0,0 +1,49 @@
+function toggleContent() {
+    const content = document.getElementById("hiddenContent");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+// 원형 그래프함수
+function drawPieChart() {
+    const ctx = document.getElementById('myPieChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green'],
+            datasets: [{
+                label: 'Sample Data',
+                data: [12, 19, 3, 5],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+            }
+        }
+    });
+}
+
+// 페이지가 로드될 때 그래프 그림.
+window.onload = function() {
+    drawPieChart();
+};
